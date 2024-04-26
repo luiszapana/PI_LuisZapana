f0a6712c3ef8be81ff31638ae89fa1f91013cabd
luiszapana <luis.al.zapana@gmail.com>
2024-04-26 13:10:54 -0300

    Experiencia + Educacion

--- PI_LuisZapana/src/app/app.module.ts
+++ PI_LuisZapana/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoLuisComponent } from './components/logo-luis/logo-luis.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoLuisComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
