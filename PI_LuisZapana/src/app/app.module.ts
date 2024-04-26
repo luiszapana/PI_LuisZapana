3ec45233d1d1443a34cce042d7351a28402d4e6d
luiszapana <luis.al.zapana@gmail.com>
2024-04-26 09:30:40 -0300

    falta instalar bootstrap

--- PI_LuisZapana/src/app/app.module.ts
+++ PI_LuisZapana/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoLuisComponent } from './components/logo-luis/logo-luis.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoLuisComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
