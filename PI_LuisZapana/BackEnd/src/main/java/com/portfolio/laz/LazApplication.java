9caed31a1535c10675a68943bf857e90f9c5bf8b
luiszapana <luis.al.zapana@gmail.com>
2024-04-29 11:34:58 -0300

    Creación de la carpeta front y back 2

---
+++ PI_LuisZapana/BackEnd/src/main/java/com/portfolio/laz/LazApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.laz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazApplication.class, args);
+	}
+
+}
---
+++ PI_LuisZapana/BackEnd/src/test/java/com/portfolio/laz/LazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.laz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
